6b6a987b50ba18e695fbbff61f3d995b1e3f210c
Marcelo Fabian Lopez Castro <marcelofabianlopezcastro@gmail.com>
2025-07-09 16:57:22 -0300

    Agregado archivo con función y actualización de formulario interactivo

---
+++ primeros_scripts/formulario_función.py
@@ -0,0 +1,9 @@
+# El ejercicio convertido a función
+
+def saludar_usuario():
+    nombre = input("¿Cómo te llamás?")
+    edad = input("¿Cuántos años tenés?")
+    ciudad = input("¿Dónde vivís?")
+    print(f"¡Hola {nombre}, tenés {edad} años y vivís en {ciudad}, un gusto conocerte!")
+    
+saludar_usuario()
--- primeros_scripts/formulario_interactivo.py
+++ primeros_scripts/formulario_interactivo.py
@@ -2,11 +2,11 @@
 
 # Solicitamos los nombres al usuario
 
-nombre = input("¿Cómo te llamás?")
+nombre = input("¿Cómo te llamás?") # esto es una variable, almacena info ingresada
 edad = input("¿Cuántos años tenés?")
 ciudad = input("¿Dónde vivís?")
 
 # Mostramos un mensaje personalizado usando f-strings
 
-print(f"Hola {nombre}, de {edad} años, qué bueno que vivas en {ciudad}.")
+print(f"Hola {nombre}, de {edad} años, qué bueno que vivas en {ciudad}.") #f string concatena texto y variables
 
