17087c94d86445cbbd8257a352a755c7404ef083
Marcelo Fabian Lopez Castro <marcelofabianlopezcastro@gmail.com>
2025-07-09 13:21:24 -0300

    foem

---
+++ primeros_scripts/formulario_interactivo.py
@@ -0,0 +1,12 @@
+# formulario_interactivo.py"
+
+# Solicitamos los nombres al usuario
+
+nombre = input("¿Cómo te llamás?")
+edad = input("¿Cuántos años tenés?")
+ciudad = input("¿Dónde vivís?")
+
+# Mostramos un mensaje personalizado usando f-strings
+
+print(f"Hola {nombre}, de {edad} años, qué bueno que vivas en {ciudad}.")
+
