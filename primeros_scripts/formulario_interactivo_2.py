b36542bdd7424e2ef3ffe7e4c78f56cc8336235b
Marcelo Fabian Lopez Castro <marcelofabianlopezcastro@gmail.com>
2025-07-23 21:06:56 -0300

    jsj

---
+++ primeros_scripts/formulario_interactivo_2.py
@@ -0,0 +1,9 @@
+#se pide nombre, edad y lugar
+
+nombre = input("¿Cuál es tu nombre?")
+edad = input("¿Cuántos años tenés?")
+lugar = input("¿Dónde vivís?")
+
+print(f"Hola, {nombre}, de {edad} años, ¿así que vivís en {lugar}? ¡Genial!")
+
+
