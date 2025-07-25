cedacaf5a4484ed4becf4b353945a0a5a76b473c
Marcelo Fabian Lopez Castro <marcelofabianlopezcastro@gmail.com>
2025-07-25 20:11:28 -0300

    vv

---
+++ primeros_scripts/1_100.py
@@ -0,0 +1,8 @@
+#Escribir un programa que sume los primeros 100 números naturales usando un bucle.
+
+
+
+for i in range(1,101):
+    print(i)
+    i =  + 1    
+    
---
+++ primeros_scripts/tarea_numero_positivo.py
@@ -0,0 +1,10 @@
+numero = int(input("decime un número "))
+
+if numero > 0:
+    print("es positivo")
+elif numero == 0:
+    print("es cero")
+else:
+    print("es negativo")
+    
+    
